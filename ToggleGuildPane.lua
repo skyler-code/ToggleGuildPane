290e3671353999350ea88bcf3b75d8625e71e2b7
skylerh15 <skylerh15@users.noreply.github.com>
2018-07-20 21:17:20 -0500

    Uses the true functionality of the old Show Guild Pane keybind

--- ToggleGuildPane.lua
+++ ToggleGuildPane.lua
@@ -1,14 +1,24 @@
 BINDING_NAME_TOGGLEGUILDPANE = "Toggle Guild Pane"
 function ToggleGuildPane()
-	if( GuildFrame and GuildFrame:IsShown() ) then
-		HideUIPanel(GuildFrame);
-	else
-		if ( IsInGuild() ) then
-			GuildFrame_LoadUI();
-			if ( GuildFrame ) then
-				GuildFrameTab2:Click();
-				ShowUIPanel(GuildFrame);
-			end
+	if (IsKioskModeEnabled()) then
+		return;
+	end
+
+	local factionGroup = UnitFactionGroup("player");
+	if (factionGroup == "Neutral") then
+		return;
+	end
+
+	if ( IsTrialAccount() or (IsVeteranTrialAccount() and not IsInGuild()) ) then
+		UIErrorsFrame:AddMessage(ERR_RESTRICTED_ACCOUNT_TRIAL, 1.0, 0.1, 0.1, 1.0);
+		return;
+	end
+	if ( IsInGuild() ) then
+		GuildFrame_LoadUI();
+		if ( GuildFrame_Toggle ) then
+			GuildFrame_Toggle();
 		end
+	else
+		ToggleGuildFinder();
 	end
 end
